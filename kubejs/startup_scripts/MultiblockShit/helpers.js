c3e5272b81f43358019deaed7c77bd9acf75ecd2
Nexian-Archonite <nohoe39085@gmail.com>
2026-07-29 19:36:23 +0200

    4000 lines fuckers in materials.js!

--- kubejs/startup_scripts/MultiblockShit/helpers.js
+++ kubejs/startup_scripts/MultiblockShit/helpers.js
@@ -185,3 +185,13 @@ global.blastProperty = (material, temperature, gasTier, voltage, duration) => {
     let mat = GTMaterials.get(material)
     mat.setProperty(PropertyKey.BLAST, new BlastProperty(temperature, gasTier, voltage, duration, -1, -1))
 }
+
+global.coilMachineTempDisplay = (controller, components) => {
+    if (controller instanceof $CoiledMulti && controller.isFormed()) {
+        components.add(
+            Component.translatable("gtceu.multiblock.blast_furnace.max_temperature",
+                Component.literal(`§c${$FormattingUtil.formatNumbers(controller.getCoilType().getCoilTemperature() + 100 * Math.max(0, controller.getTier() - GTValues.MV))}K§r`)
+            )
+        );
+    }
+};
